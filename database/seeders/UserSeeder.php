634cb999c420483b36193acfa6349c8270b17416
burhanmiroj <mirojburhanudin2@gmail.com>
2026-01-26 22:51:52 +0700

    Setup basic spatie/laravel-permission

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,5 +18,7 @@ class UserSeeder extends Seeder
             'email'=> 'admin@gmail.com',
             'password' => Hash::make('12345678')
         ]);
+
+        $admin->assignRole('administrator');
     }
 }
