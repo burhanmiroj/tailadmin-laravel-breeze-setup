2684a0ea7f7d8ad0630596524862be67bfcd7284
burhanmiroj <mirojburhanudin2@gmail.com>
2026-01-29 19:18:49 +0700

    Setup google oauth

---
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Exception;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function handleGoogleCallback()
+    {
+        try {
+            $user = Socialite::driver('google')->user();
+            $finduser = User::where('google_id', $user->id)->first();
+
+            if ($finduser) {
+                Auth::login($finduser);
+
+                return redirect()->route('home');
+            } else {
+                $newUser = User::updateOrCreate(['email' => $user->email], [
+                    'name' => $user->name,
+                    'google_id' => $user->id,
+                    'password' => encrypt('12345678')
+                ]);
+
+                Auth::login($newUser);
+
+                return redirect()->route('home');
+            }
+
+        } catch (Exception $e) {
+            Log::info($e->getMessage());
+
+            abort(500);
+        }
+    }
+}
